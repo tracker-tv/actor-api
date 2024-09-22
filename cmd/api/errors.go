6ae74f4a3b44e869e3c3d7af22f3a4eff3a88b94
Florent Sorel <florent.sorel@gmail.com>
2024-09-22 14:10:30 +0200

    feat: add helpers function (#2)

---
+++ cmd/api/errors.go
@@ -0,0 +1,27 @@
+package main
+
+import "net/http"
+
+func (app *application) logError(r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message string) {
+	data := map[string]any{"error": message, "status": status}
+	err := app.writeJSON(w, status, data, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
